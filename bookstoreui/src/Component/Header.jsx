43b3e5b10da27240486dee31aedafe1e7078d57a
Somya <somyacs14@gmail.com>
2020-05-08 03:29:34 -0700

    [Somya]Add. creating the header and render Method in Component folder

---
+++ bookstoreui/src/Component/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Typography from '@material-ui/core/Typography';
+class Header extends Component {
+
+    render() {
+        return (
+
+           
+        )
+    }
+}
+export default Header;
