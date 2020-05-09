38a24620dd79bd84d1036cbda8d46ada8c8ef75e
sumitchourasia <st.sumit01@gmail.com>
2020-05-10 02:16:52 +0530

    [sumit] Refactor . shited the using directives inside namespace

--- BookStore_Backend/Model/ModelCLasses/BookModel.cs
+++ BookStore_Backend/Model/ModelCLasses/BookModel.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
-
+﻿
 namespace Model.ModelCLasses
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class BookModel
     {
         /// <summary>
