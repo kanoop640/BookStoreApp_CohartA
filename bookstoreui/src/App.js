05f2c366cc658f7278bd88006fe6193c9913abce
Somya <somyacs14@gmail.com>
2020-05-09 05:03:20 -0700

    [Somya]Refactor. refactor the header and app.css in Component

--- bookstoreui/src/App.js
+++ bookstoreui/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Component/Header'
+import Header from './Component/header'
 function App() {
   return (
     <div className="App">
--- bookstoreui/src/Component/Header.jsx
+++ bookstoreui/src/Component/Header.jsx
@@ -6,45 +6,71 @@ import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Typography from '@material-ui/core/Typography';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import InputBase from '@material-ui/core/InputBase';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
+import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
+import RefreshRoundedIcon from '@material-ui/icons/RefreshRounded';
+import ViewStreamRoundedIcon from '@material-ui/icons/ViewStreamRounded';
+import AppsRoundedIcon from '@material-ui/icons/AppsRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import ViewColumnIcon from '@material-ui/icons/ViewColumn';
+import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 class Header extends Component {
-
     render() {
         return (
-
-            <div className='header' >   
-                <div className='book-logo-search-div'>
-                    <div className='book-logo-div'>
-                    <MenuBookIcon/>
+          <div>
+            <AppBar position="static" className="AppBar">
+              <Toolbar variant="dense" className="toolbar">
+              <div className="temp" style={styles.bookIcon}>
+                                <MenuBookIcon fontSize='medium' />
+                            {/* </div>
+                            <div className='book-title'> */}
+                                <Typography variant='h6'>
+                                    Book Store
+                            </Typography>
+                            </div>
+                <div className="search_box">
+                    <div className="searchIcon"
+                      title="Search"
+                    >
+                      <SearchIcon />
                     </div>
-                    <div className='book-tittle'>
-                    <Typography variant='h4'>
-                        Book Store
-                    </Typography>
-                    </div>
-                    <div className='search-div'>
-                    <TextField
-                     className='search-textfield'
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <SearchIcon />
-                            </InputAdornment>
-                        ),
-                    }} 
-                    id="outlined-basic" 
-                     />
-                    </div>
-                    </div> 
-                <div className='cart-wishlist-div'>
-                <div className='cart-div'>
-                <ShoppingCartIcon/> 
+                    <InputBase className="input_search"
+                      type="searchIcon"
+                      placeholder="Search..."
+                      />
                 </div>
-                <div className='wishlist-div'>
-                <FavoriteIcon/>
-                </div>
-                </div>
-            </div>
+                <div style={styles.rightIcons}>
+                                <FavoriteBorderOutlinedIcon fontSize='medium' />
+                                <ShoppingCartOutlinedIcon fontSize='medium' />
+                        </div>
+              </Toolbar>
+            </AppBar>
+           
+          </div>
         )
+      }
+    
     }
-}
-export default Header;
+    const styles = {
+        bookIcon:{
+            width:300,
+            marginLeft:100,
+            display:"flex"
+        },
+        cartIcon:{
+            marginLeft:90,
+        },
+        rightIcons: {
+            width:100,
+            display:"flex",
+            justifyContent: "space-around"
+        }
+      }
+export default Header
