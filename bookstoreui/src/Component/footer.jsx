0abcda3866cf142e20499006b4a38a153f70410d
Anjalik19 <anjali.march19.com>
2020-05-09 23:21:44 +0530

    [Anjali]Refactor. Refactor the code.and added css

--- bookstoreui/src/Component/footer.jsx
+++ bookstoreui/src/Component/footer.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from "react"
 import Pagination from '@material-ui/lab/Pagination';
 import { withRouter } from 'react-router-dom';
-export class Footer extends Component{
-    constructor(props){
+export class Footer extends Component {
+    constructor(props) {
         super();
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <div className="foot">
-<p>Copyright &copy; 2020. BookStore Pvt. Ltd.</p>
-</div>
+                    <p>Copyright &copy; 2020. BookStore Pvt. Ltd.</p>
+                </div>
             </div>
         )
     }
