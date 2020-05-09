3dd841b7b63e739c6336e59e69d30f1b6afd7ded
Anjalik19 <anjali.march19.com>
2020-05-09 23:18:43 +0530

    [Anjali]Refactor. Refactor the code. added event handling for getAllBook

--- bookstoreui/src/Component/dashboard.jsx
+++ bookstoreui/src/Component/dashboard.jsx
@@ -1,136 +1,41 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import BookDashboard from './bookDashboard';
 import Header from './header'
 import dragon from '../assets/dragon.jpg'
 import Footer from './footer'
+import { getBook } from '../Service/service'
 
-class Dashboard extends Component{
-    state={
-        books:[
-        {
-            bookid : 1,
-            image:dragon,
-            bookName : "Shero",
-            authorName : "shero",
-            price : 500,
-            description:"You can always identify young Tiny Dragons by the eggshells they wear on their head.",
+class Dashboard extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            bookId: '',
+            title: '',
+            author: '',
+            image: '',
+            price: '',
+            description: '',
+            result: [],
 
-        },
-        {
-            bookid : 2,
-            image:dragon,
-            bookName : "Jimmy",
-            authorName : "jimmy",
-            price : 400,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 3,
-            image:dragon,
-            bookName : "Sumit",
-            authorName : "Sumit",
-            price : 550,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 4,
-            image:dragon,
-            bookName : "Somya",
-            authorName : "Somya",
-            price : 400,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 5,
-            image:dragon,
-            bookName : "Ankit",
-            authorName : "Ankit",
-            price : 500,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 6,
-            image:dragon,
-            bookName : "Rajat",
-            authorName : "Rajat",
-            price : 400,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 7,
-            image:dragon,
-            bookName : "Anjali",
-            authorName : "Anjali",
-            price : 450,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },
-        {
-            bookid : 1,
-            image:dragon,
-            bookName : "Shero",
-            authorName : "shero",
-            price : 500,
-            description:"You can always identify young Tiny Dragons by the eggshells they wear on their head.",
-
-        },
-        {
-            bookid : 2,
-            image:dragon,
-            bookName : "Jimmy",
-            authorName : "jimmy",
-            price : 400,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 3,
-            image:dragon,
-            bookName : "Sumit",
-            authorName : "Sumit",
-            price : 550,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 4,
-            image:dragon,
-            bookName : "Somya",
-            authorName : "Somya",
-            price : 400,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 5,
-            image:dragon,
-            bookName : "Ankit",
-            authorName : "Ankit",
-            price : 500,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 6,
-            image:dragon,
-            bookName : "Rajat",
-            authorName : "Rajat",
-            price : 400,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },{
-            bookid : 7,
-            image:dragon,
-            bookName : "Anjali",
-            authorName : "Anjali",
-            price : 450,
-            description:"No matter how old it gets, the Tiny Dragon is always a baby at heart. You can always identify young Tiny Dragons by the eggshells they wear on their head."
-
-        },
-    ]
+        }
     }
-    render(){
-        return(
-            <div styles={{flexDirection:"column", justifyContent:"flex-start", alignItems:"end", flex:1 }}>
-                <Header/>
-            <BookDashboard books={this.state.books}/>
-            <Footer styles={{position:"fixed"}}/>
+        
+   func =async() =>{
+           console.log("test")
+            let result = await getBook()
+            this.setState({result})
+        }
+    componentDidMount() {
+        this.func();
+            }
+
+    render() {
+
+        return (
+            <div>
+                <Header />
+                <BookDashboard books={this.state.result} />
+                <Footer />
             </div>
         )
 
