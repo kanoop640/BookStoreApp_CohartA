6f72b499f043b91a68a9eacc78f6b4b835c8cfc6
Somya <somyacs14@gmail.com>
2020-05-09 08:08:39 -0700

    [Somya]Add. adding the MyCart.jsx in Component folder

--- bookstoreui/src/Component/MyCart.jsx
+++ bookstoreui/src/Component/MyCart.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
 class Mycart extends Component {
 
     render() {
         return (
-         <div>
-            <div className='Mycart' >
-               <button className="button">
-
-                   </button>
-               </div>
-            <div className="CostomerDetails">
-             edfvdfvdfbdvb
+            <div>
+                <div className='Mycart' >
+                    <Button className="button"
+                        variant="contained" color="primary">
+                        Place Order
+                    </Button>
+                </div>
+                <div className="CostomerDetails">
+                    
             </div>
 
-            <div className="Ordersummary">
-             xfbdvgn cbn fh b
-            </div>  
-            </div>  
-      )
+                <div className="Ordersummary">
+                    
+            </div>
+            </div>
+        )
     }
 }
 export default Mycart;
