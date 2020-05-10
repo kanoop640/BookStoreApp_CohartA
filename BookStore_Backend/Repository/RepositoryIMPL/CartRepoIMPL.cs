35ec392d0332509b6338509b7ad4f67ba638fbf5
sumitchourasia <st.sumit01@gmail.com>
2020-05-10 10:17:32 +0530

    [sumit] Refactor . provided the implementation of cart specific methods of ICartRepo interface

--- BookStore_Backend/Repository/RepositoryIMPL/CartRepoIMPL.cs
+++ BookStore_Backend/Repository/RepositoryIMPL/CartRepoIMPL.cs
@@ -3,12 +3,15 @@
 /// </summary>
 namespace Repository.RepositoryIMPL
 {
+    using Microsoft.AspNetCore.Mvc;
     using Model.ModelCLasses;
     using Repository.Context;
     using Repository.IRepository;
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+   
 
     /// <summary>
     /// Implementation class for ICartRepo interface
@@ -34,34 +37,55 @@ namespace Repository.RepositoryIMPL
         /// </summary>
         /// <param name="CartItem"></param>
         /// <returns>int</returns>
-        public Task<int> AddCartItem(CartModel CartItem)
+        public  Task<int> AddCartModel(CartModel cartModel)
         {
-            this._context.CartContext.Add(CartItem);
-            var result = this._context.SaveChangesAsync();
+            this._context.CartContext.Add(cartModel);
+            var result =  this._context.SaveChangesAsync();
             return result;
         }
 
-        /// <summary>
-        /// Delete Cart item from CartContext
-        /// </summary>
-        /// <param name="BookId"></param>
-        /// <returns></returns>
-        public CartModel DeleteCartItem(long BookId)
+        public Task<int> DeleteCartModel(long BookId)
         {
             CartModel cartModel = this._context.CartContext.Find(BookId);
             if (cartModel != null)
             {
-                this._context.CartContext.Remove(cartModel);
-                this._context.SaveChanges();
+                _context.CartContext.Remove(cartModel);
+               var result = this._context.SaveChanges();
+                return Task.FromResult(result);
             }
-            return cartModel;
+            return Task.FromResult(0);
         }
+
+        /*/// <summary>
+        /// Delete Cart item from CartContext
+        /// </summary>
+        /// <param name="BookId"></param>
+        /// <returns></returns>
+        public async Task<int> DeleteCartModel(long BookId)
+        {
+            int result=0;
+            var cartModel = await _context.CartContext.FindAsync(BookId);
+            if (cartModel != null)
+            {
+                _context.CartContext.Remove(cartModel);
+                result = await _context.SaveChangesAsync();
+            }
+            return await Task.FromResult(result);
+
+            *//*CartModel cartModel = await this._context.CartContext.FindAsync(BookId);
+            if (cartModel != null)
+            {
+                this._context.CartContext.Remove(cartModel);
+                await this._context.SaveChangesAsync();
+            }
+            return *//*
+        }*/
 
         /// <summary>
         /// Get All Cart Item from CartContext
         /// </summary>
         /// <returns></returns>
-        public IEnumerable<CartModel> GetAllCartItem()
+        public IEnumerable<CartModel> GetCartContext()
         {
              return  this._context.CartContext.ToList();
         }
@@ -72,13 +96,25 @@ namespace Repository.RepositoryIMPL
         /// <param name="OldBookDetails"></param>
         /// <param name="NewBookDetails"></param>
         /// <returns></returns>
-        public Task<int> UpdateCartItem(CartModel OldBookDetails, CartModel NewBookDetails)
+        public async Task<int> UpdateCartModel(CartModel newCartModel)
         {
-            OldBookDetails.Count  = NewBookDetails.Count;
-            if (OldBookDetails.Count <= 0)
-                DeleteCartItem( OldBookDetails.BookId );
-            var result = this._context.SaveChangesAsync();
-            return result; 
+            try
+            {
+                var oldCartModel = await _context.CartContext.FindAsync(newCartModel.CartId);
+                oldCartModel.Count = newCartModel.Count;
+             /*   if (OldBookDetails.Count <= 0)
+                    DeleteCartModel(OldBookDetails.BookId);*/
+                return await this._context.SaveChangesAsync();
+            }
+            catch(Exception)
+            {
+                return await Task.FromResult(0); 
+            }
+        }
+
+        Task<IActionResult> ICartRepo.DeleteCartModel(long BookId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
