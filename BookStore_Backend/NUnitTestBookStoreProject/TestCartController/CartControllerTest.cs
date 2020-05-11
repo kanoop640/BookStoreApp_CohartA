029339ad1a541bf2281629299880f7ba37c29c8d
sumitchourasia <st.sumit01@gmail.com>
2020-05-11 09:03:47 +0530

    [sumit] Add . adding Nunit test for Cart Controller

---
+++ BookStore_Backend/NUnitTestBookStoreProject/TestCartController/CartControllerTest.cs
@@ -0,0 +1,23 @@
+﻿
+namespace NUnitBookStoreTestProject.TestCartController
+{
+    using NUnit.Framework;
+    using Repository.IRepository;
+    using Moq;
+    using Manager.ManagerIMPL;
+    using BookStoreWebAPI.Controllers;
+
+    [TestFixture]
+    public class CartControllerTest
+    {
+        [TestCase]
+        public void GetCartContextTest()
+        {
+            var cardRepoMock = new Mock<ICartRepo>();
+            var cartMangerMock = new CartManager(cardRepoMock.Object);
+            var cartControllerMock = new CartController(cartMangerMock);
+            var result = cartControllerMock.GetCartContext();
+            Assert.NotNull(result);
+        }
+    }
+}
