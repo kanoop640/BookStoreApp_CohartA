f5578983d70d2d6ce5485e5403d8abadfffc29f2
sumitchourasia <st.sumit01@gmail.com>
2020-05-11 06:34:16 +0530

    [Rajat] Refactor . removed previously incorrect implementation

--- BookStore_Backend/Manager/IManager/ICartManager.cs
+++ BookStore_Backend/Manager/IManager/ICartManager.cs
@@ -9,30 +9,6 @@ namespace Manager.IManager
 {
     public interface ICartManager
     {
-        /// <summary>
-        /// Declare GetAll Methods.
-        /// </summary>
-        /// <returns>AllEmployee.</returns>
-        IEnumerable<CartModel> GetAllBooksInCart();
-
-        /// <summary>
-        /// Declare Add Method.
-        /// </summary>
-        /// <param name="entity">entity.</param>
-        /// <returns>Task.</returns>
-        Task<int> AddBookToCart(CartModel BookItem);
-
-        /// <summary>
-        /// Declare Update method.
-        /// </summary>
-        /// <param name="dbEntity">dbEntity.</param>
-        /// <param name="entity">entity.</param>
-        Task<int> UpdateBookInCart(CartModel BookToUpdate, CartModel BookNewDetails);
-
-        /// <summary>
-        /// Declare Delete method.
-        /// </summary>
-        /// <param name="entity">entity.</param>
-        CartModel Delete(int BookId);
+       
     }
 }
--- BookStore_Backend/Manager/ManagerIMPL/CartManager.cs
+++ BookStore_Backend/Manager/ManagerIMPL/CartManager.cs
@@ -11,31 +11,7 @@ namespace Manager.ManagerIMPL
 {
    public class CartManager : ICartManager
     {
-        private readonly ICartManager cartManager;
-        public CartManager(ICartManager cartManager)
-        {
-            this.cartManager = cartManager;
-        }
-
-        public Task<int> AddBookToCart(CartModel BookItem)
-        {
-            return this.cartManager.AddBookToCart(BookItem);
-        }
-
-        public CartModel Delete(int BookId)
-        {
-            return this.cartManager.Delete(BookId);
-        }
-
-        public IEnumerable<CartModel> GetAllBooksInCart()
-        {
-            return this.cartManager.GetAllBooksInCart();
-        }
-
-        public Task<int> UpdateBookInCart(CartModel BookToUpdate, CartModel BookNewDetails)
-        {
-            return this.cartManager.UpdateBookInCart(BookToUpdate, BookNewDetails);
-        }
+      
 
     }
 }
