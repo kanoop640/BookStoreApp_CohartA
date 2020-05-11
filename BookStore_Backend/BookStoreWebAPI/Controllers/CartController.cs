2cb30e61e320d939c0ae9f323292c8510e9fd47f
sumitchourasia <st.sumit01@gmail.com>
2020-05-11 06:20:18 +0530

    [Rajat] Add . added CartController nad constructor

--- BookStore_Backend/BookStoreWebAPI/Controllers/CartController.cs
+++ BookStore_Backend/BookStoreWebAPI/Controllers/CartController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Manager.IManager;
+using Model.ModelCLasses;
 
 namespace BookStoreWebAPI.Controllers
 {
@@ -11,5 +13,12 @@ namespace BookStoreWebAPI.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly ICartManager _cartManager;
+
+        public CartController(ICartManager cartManager)
+        {
+            this._cartManager = cartManager;
+        }
+       
     }
 }
