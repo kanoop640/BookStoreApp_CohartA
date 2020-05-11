9498a28c7aa56c76ffa3252417a1ac8a044cd663
sumitchourasia <st.sumit01@gmail.com>
2020-05-11 09:03:09 +0530

    [sumit] Add . adding Nunit test for Book Controller

---
+++ BookStore_Backend/NUnitTestBookStoreProject/TestBookController/BookControllerTest.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+namespace NUnitBookStoreTestProject.TestBookController
+{
+    using NUnit.Framework;
+    using Repository.IRepository;
+    using Moq;
+    using Manager.ManagerIMPL;
+    using BookStoreWebAPI.Controllers;
+
+    [TestFixture]
+    public class BookControllerTest 
+    {
+        [TestCase]
+        public void GetAllBookTest()
+        {
+            var bookrepomock = new Mock<IBookRepo>();
+            var bookmangermock = new BookManager(bookrepomock.Object);
+            var BookControllermock = new BookController(bookmangermock);
+            var result = BookControllermock.GetAllBook();
+            Assert.NotNull(result);
+        }
+    }
+}
