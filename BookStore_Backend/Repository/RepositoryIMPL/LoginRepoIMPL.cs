b794a51775829fa9103cd8df5e411ea57f378b03
sumitchourasia <st.sumit01@gmail.com>
2020-05-12 07:28:34 +0530

    [sumit] Add . added LoginRepoIMPL and abstract methods for login and signup

---
+++ BookStore_Backend/Repository/RepositoryIMPL/LoginRepoIMPL.cs
@@ -0,0 +1,59 @@
+﻿using Model.ModelCLasses;
+using Repository.Context;
+using Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.RepositoryIMPL
+{
+    /// <summary>
+    /// LoginRepo class extends ILoginRepo
+    /// </summary>
+    public class LoginRepoIMPL : ILoginRepo
+    {
+        private readonly UserDBContext _context;
+
+        /// <summary>
+        /// COntructor
+        /// </summary>
+        /// <param name="context"></param>
+        public LoginRepoIMPL(UserDBContext context)
+        {
+            this._context = context;
+        }
+
+        /// <summary>
+        /// Login method implementation
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
+        public bool LogIn(LogInModel loginModel)
+        {
+            LogInModel _loginModel = this._context.LoginContext.Find(loginModel.Email);
+            if(_loginModel.Email == loginModel.Email)
+            {
+                if(_loginModel.Password==loginModel.Password)
+                {
+                    return true ;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// SignUp Method Implementation
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
+        public bool SignUp(LogInModel loginModel)
+        {
+              this._context.LoginContext.Add(loginModel);
+            var result = this._context.SaveChanges();
+            if(result != 0)
+                return true;
+            return false;
+        }
+    }
+}
