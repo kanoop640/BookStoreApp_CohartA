ffe78786b3e563c1a44c23ab210e2eee5b9607cb
sumitchourasia <st.sumit01@gmail.com>
2020-05-12 07:22:05 +0530

    [sumit] Add . added the LoginModel class

--- BookStore_Backend/Model/ModelCLasses/LoginModel.cs
+++ BookStore_Backend/Model/ModelCLasses/LoginModel.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Model.ModelCLasses
 {
-    public class LoginModel
+    /// <summary>
+    /// model class for login
+    /// </summary>
+    public class LogInModel
     {
-
+        /// <summary>
+        /// property for Email
+        /// </summary>
+        [Key] 
         public string Email { get; set; }
 
+        /// <summary>
+        /// property for Password
+        /// </summary>
         public string Password { get; set; }
 
     }
