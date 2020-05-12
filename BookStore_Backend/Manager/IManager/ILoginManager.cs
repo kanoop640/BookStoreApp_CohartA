94271cdfb147055e6a139f50a0aab189d0fe4f18
sumitchourasia <st.sumit01@gmail.com>
2020-05-12 07:25:33 +0530

    [sumit] Add . added ILoginManager in IManager along with login and signUp methods

---
+++ BookStore_Backend/Manager/IManager/ILoginManager.cs
@@ -0,0 +1,25 @@
+﻿using Model.ModelCLasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.IManager
+{
+    public interface ILoginManager
+    {
+        /// <summary>
+        ///abstract method to signup
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
+         bool SignUp(LogInModel loginModel);
+
+        /// <summary>
+        /// abstract method to login
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
+         bool LogIn(LogInModel loginModel);
+    }
+}
