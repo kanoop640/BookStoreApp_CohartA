0def1b18ce6070039e0e1053b5fdc7463fbfd273
sumitchourasia <st.sumit01@gmail.com>
2020-05-12 07:26:46 +0530

    [sumit] Add . added the LoginManager class and also methods for Login and Signup

---
+++ BookStore_Backend/Manager/ManagerIMPL/LoginManager.cs
@@ -0,0 +1,31 @@
+﻿using Manager.IManager;
+using Model.ModelCLasses;
+using Repository.Context;
+using Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.ManagerIMPL
+{
+    public class LoginManager : ILoginManager
+    {
+        private readonly ILoginRepo _loginRepo;
+
+        public LoginManager(ILoginRepo loginRepo)
+        {
+            this._loginRepo = loginRepo;
+        }
+
+        public bool LogIn(LogInModel loginModel)
+        {
+           return  this._loginRepo.LogIn(loginModel);
+        }
+
+        public bool SignUp(LogInModel loginModel)
+        {
+            return this._loginRepo.SignUp(loginModel);
+        }
+    }
+}
