b076a6c2babb49c5fa538365ec0532efe891b048
sumitchourasia <st.sumit01@gmail.com>
2020-05-12 08:24:58 +0530

    [sumit] Refactor . renamed the method name

--- BookStore_Backend/NUnitTestBookStoreProject/TestBookController/BookControllerTest.cs
+++ BookStore_Backend/NUnitTestBookStoreProject/TestBookController/BookControllerTest.cs
@@ -8,6 +8,7 @@ namespace NUnitBookStoreTestProject.TestBookController
     using Moq;
     using Manager.ManagerIMPL;
     using BookStoreWebAPI.Controllers;
+    using Model.ModelCLasses;
 
     [TestFixture]
     public class BookControllerTest 
@@ -19,6 +20,28 @@ namespace NUnitBookStoreTestProject.TestBookController
             var bookmangermock = new BookManager(bookrepomock.Object);
             var BookControllermock = new BookController(bookmangermock);
             var result = BookControllermock.GetAllBook();
+            Assert.NotNull(result);
+        }
+
+        [TestCase]
+        public void AddBookTest()
+        {
+            var bookrepomock = new Mock<IBookRepo>();
+            var bookmangermock = new BookManager(bookrepomock.Object);
+            var BookControllermock = new BookController(bookmangermock);
+            BookModel bookModel = new BookModel()
+            {
+                BookId = 0,
+                Author = "sumit",
+                Title = "Sumit-Biograpy",
+                AvailableBooks = 5,
+                Price = 50000,
+                Image = "imageurl",
+                Description = "asdfghj wertyu sdvb tgb ikm ",
+                Ratings = "5 star",
+                Review = "wqer  dvzs sd rht tyjh nfg " 
+            };
+            var result = BookControllermock.AddBook(bookModel);
             Assert.NotNull(result);
         }
     }
