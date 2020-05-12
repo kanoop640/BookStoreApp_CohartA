376db88e741e0fa8c26e7888b9b014902bae858c
Somya <somyacs14@gmail.com>
2020-05-12 10:37:22 -0700

    [Somya]Refactor. refactor the Mycart.jsx in Component folder of  bookDetails

--- bookstoreui/src/Component/MyCart.jsx
+++ bookstoreui/src/Component/MyCart.jsx
@@ -9,45 +9,82 @@ class Mycart extends Component {
 
         return (
             <div>
-                <div className='Mycart' >
-                    <button class="button">Place Order</button>
+            <div className='Mycart' >
+                <div className="s">
+                    My Cart (2)
                 </div>
 
-                <div className="CostomerDetails" >
-                    Customer Details
-
-                    <div classNmae="onclick">
-
-                            <div className="details">
-                                <div classNmae="hii">
-                                <TextField id="outlined-basic" label="Name" variant="outlined" />
-                                </div>
-                                <div  classNmae="hi">
-                                <TextField id="outlined-basic" label="Phoneno" variant="outlined" />
-                                </div>
-                            </div>
-
-
-
-                        <div className="Pincode">
-                        <TextField id="outlined-basic" label="Pincode" variant="outlined" />
-                        <TextField id="outlined-basic" label="Locality" variant="outlined" />
-                        </div>
-                        <div>
-                        <input  placeholder="Address" className="Pin" type="text">
-
-
-                        </input>
-                       </div>
-                        <div className="Pincode">
-                        <TextField id="outlined-basic" label="CityTown" variant="outlined" />
-                        <TextField id="outlined-basic" label="Landmark" variant="outlined" />
-                        </div>
-                        <button class="button1">CONTINUE</button>
-                    </div>
+                <div className="incrementDecrement">
+                <button onClick="add()" className="c">+</button>
+                <input type="text" id="somya" className="b"/>
+                <button onClick="sub()" className="c">-</button>
+                {/* <button className="d">Remove</button> */}
+                <Button color="primary">Remove</Button>
                 </div>
 
+                <button class="button">Place Order</button>
+               
             </div>
+
+            <div className="CostomerDetails" >
+                <div className="yss">
+                Customer Details
+                </div>
+                <div className="onclick">
+
+                    <div className="details">
+                        <div >
+                            <TextField id="outlined-basic" label="Name" variant="outlined" />
+                        </div>
+                        <div className="space">
+                            <TextField id="outlined-basic" label="Phoneno." variant="outlined" />
+                        </div>
+                    </div>
+
+                    <div className="Pincode">
+                        <div  >
+                            <TextField id="outlined-basic" label="Pincode" variant="outlined" />
+                        </div>
+                        <div className="space" >
+                            <TextField id="outlined-basic" label="Locality" variant="outlined" />
+                        </div>
+                    </div>
+
+
+                    <div className="Pincode">
+                        <TextField id="outlined-basic" label="Address" variant="outlined"  className="fullsize"/>
+                    </div>
+
+                    <div  className="Pincode">
+                    <div>
+                        <TextField id="outlined-basic" label="CityTown" variant="outlined" />
+                    </div>
+                    <div   className="space">
+                        <TextField id="outlined-basic" label="Landmark" variant="outlined" />
+                    </div>
+                    </div>
+                   <div className="type">
+                    Type.....
+                   </div>
+                   <div className="sss">
+                   <div>
+                   <button onClick="sub()" className="e"></button>
+                   Home
+                   </div>
+                   <div>
+                   <button onClick="sub()" className="e"></button>
+                   Work
+                   </div>
+                   <div>
+                   <button onClick="sub()" className="e"></button>
+                   Other
+                   </div>
+                   </div>
+                    <button class="button1">CONTINUE</button>
+                </div>
+            </div>
+
+        </div>
         )
     }
 }
@@ -58,49 +95,3 @@ export default Mycart;
 
 
 
-// import React from 'react';
-// import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-// import TextField from '@material-ui/core/TextField';
-
-// const useStyles = makeStyles((theme: Theme) =>
-//     createStyles({
-//         root: {
-//             '& > *': {
-//                 margin: theme.spacing(1),
-//                 width: '25ch',
-//             },
-//         },
-//     }),
-// );
-
-// export default function Mycart() {
-//     const classes = useStyles();
-
-//     return (
-//         <div>
-//             <div className='Mycart' >
-//                 <button class="button">Place Order</button>
-//             </div>
-//             <div className="CostomerDetails" >
-//                 Customer Details  
-//                 <div className="details">
-//                     <form className={classes.root} noValidate autoComplete="off">
-//                         <TextField id="outlined-basic" label="Name" variant="outlined" />
-//                         <TextField id="outlined-basic" label="Phoneno" variant="outlined" />
-//                         <TextField id="outlined-basic" label="Pincode" variant="outlined" />
-//                         <TextField id="outlined-basic" label="Locality" variant="outlined" />
-//                         <div className="Pincode">
-//                             <input placeholder="Address" className="Pin" type="text">
-//                             </input>
-//                         </div>
-//                         <TextField id="outlined-basic" label="CityTown" variant="outlined" />
-//                           <TextField id="outlined-basic" label="Landmark" variant="outlined" />
-//                         <div>
-//                          <button class="button1">CONTINUE</button>
-//                      </div>
-//                     </form>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
