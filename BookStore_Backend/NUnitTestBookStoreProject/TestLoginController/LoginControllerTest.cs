0b8bc40c4ddd07ad60dbaa7a1caa12b1c8043f92
sumitchourasia <st.sumit01@gmail.com>
2020-05-12 08:27:32 +0530

    [sumit] Add . added Test for LoginController and also two test method

---
+++ BookStore_Backend/NUnitTestBookStoreProject/TestLoginController/LoginControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using Moq;
+using Repository.IRepository;
+using Manager.ManagerIMPL;
+using BookStoreWebAPI.Controllers;
+using NUnit.Framework;
+using Model.ModelCLasses;
+
+namespace NUnitBookStoreTestProject.TestLoginController
+{
+    [TestFixture]
+    public class LoginControllerTest
+    {
+        [TestCase]
+        public void LoginTest()
+        {
+            var loginRepoMock = new Mock<ILoginRepo>();
+            var loginMangerMock = new LoginManager(loginRepoMock.Object);
+            var cartControllerMock = new LoginController(loginMangerMock);
+
+            LogInModel loginModel = new LogInModel()
+            {
+                Email = "st.sumit01@gmail.com",
+                Password = "123456789"
+            };
+
+            var result = cartControllerMock.LogIn(loginModel);
+            Assert.NotNull(result);
+        }
+
+        [TestCase]
+        public void SignUpTest()
+        {
+            var loginRepoMock = new Mock<ILoginRepo>();
+            var loginMangerMock = new LoginManager(loginRepoMock.Object);
+            var cartControllerMock = new LoginController(loginMangerMock);
+
+            LogInModel loginModel = new LogInModel()
+            {
+                Email = "st.sumit01@gmail.com",
+                Password = "123456789"
+            };
+
+            var result = cartControllerMock.SignUp(loginModel);
+            Assert.NotNull(result);
+        }
+
+    }
+}
