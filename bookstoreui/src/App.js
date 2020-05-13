8ba65a8364906de5d023867d9019c6eefcc4a74a
Somya <somyacs14@gmail.com>
2020-05-13 07:38:31 -0700

    [Somya]Refactor. resolve the merge conflict in app.css

--- bookstoreui/src/App.js
+++ bookstoreui/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Dashboard from './Component/dashboard'
 import './App.css';
 import OrderSummary from './Component/orderSummary'
+import Mycart from './Component/MyCart';
 function App() {
   
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
       <Route path='/dashboard' component={Dashboard}></Route>
       <Route path='/orderSummary' component={OrderSummary}></Route>
+      <Route path='/MyCart' component={Mycart}></Route>
       </Switch>
     </Router>
       )
